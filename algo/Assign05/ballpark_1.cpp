269ddb32f19efa5ddecdf916b1df5886b1a29ac3
JaeHeyK <jaeheyk@gmail.com>
2021-03-18 00:00:53 +0900

    Add Assign05 and solution(using for loop)

---
+++ algo/Assign05/ballpark_1.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int tripleMax(int a, int b, int c) {
+  if(a < b) {
+    return (b < c ? c : b);
+  } else {
+    return (a < c ? c : a);
+  }
+}
+
+int main() {
+  ifstream in("ballpark.inp");
+  ofstream out("ballpark.out");
+
+  int height, width;
+
+  in >> height >> width;
+
+  int** field = new int*[height];
+   for(int row = 0; row < height; row++) {
+    field[row] = new int[width];
+  }
+  
+  // int field[height][width];
+
+  string rowString;
+
+  for(int row = height - 1; row >= 0; row--) {
+    in >> rowString;
+
+    for(int col = 0; col < width; col++) {
+      field[row][col] = (int)(rowString[col] - 48)-1;
+    }
+  }
+
+  int minusMaxRadius = 0;
+
+  for(int pivotY = height-2; pivotY >= 0; pivotY--) {
+    for(int pivotX = width-2; pivotX >=0; pivotX--) {
+      if (field[pivotY][pivotX] <= -1) {
+        int currentMax = tripleMax(field[pivotY+1][pivotX], field[pivotY][pivotX+1], field[pivotY+1][pivotX+1]) - 1;
+
+        field[pivotY][pivotX] = currentMax;
+        
+        minusMaxRadius = (minusMaxRadius < currentMax ? minusMaxRadius : currentMax);
+        }
+    }
+  }
+
+  vector<pair<int,int>> maxAreaPivots;
+  int maxAreaCount = 0;
+
+  for(int pivotX = 0; pivotX < width; pivotX++) {
+    for(int pivotY = 0; pivotY < height; pivotY++) {
+      if(field[pivotY][pivotX] == minusMaxRadius) {
+        maxAreaPivots.push_back(make_pair(pivotX, pivotY));
+        maxAreaCount++;
+      }
+    }
+  }
+
+  out << -minusMaxRadius << " " << maxAreaCount << "\n";
+  for(auto pivot : maxAreaPivots) {
+    out << pivot.first+1 << " " << pivot.second+1 << "\n";
+  }
+  
+
+  for(int row = 0; row < height; row++) {
+    delete[] field[row];
+  }
+  delete[] field;
+
+  
+  in.close();
+  out.close();
+  
+  return 0;
+}
+
