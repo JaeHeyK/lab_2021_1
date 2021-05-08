3c37dd68039547fd232dd6670cad191bfa93e4cb
JaeHeyK <jaeheyk@gmail.com>
2021-05-09 01:51:01 +0900

    [Algo] Reduce Sol09-1 code size

--- algo/Assign09/marathon.cpp
+++ algo/Assign09/marathon.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int i,r,c;
-int graph[26][26] = {0,};
-int vst[26] = {0,};
-string ans="";
+int graph[26][26]={0,};
+int vst[26]={0,};
+string ans;
 
 void dfs(int nd, int dst, string tr) {
-  for(int c=25; c>=0; c--) {
+  for(int c=25;c>=0;c--) {
     int w = graph[nd][c];
     if(w!=0 && vst[c]==0){
       if(dst+w<42){
@@ -27,23 +26,21 @@ int main() {
   ifstream in("marathon.inp");
   ofstream out("marathon.out");
 
-  int N, M;
+  int N,M;
   in >> N >> M;
 
-  char s, e;
+  char s,e;
   int w;
-  for(i=0; i<M; i++) {
-    in >> s >> e >> w;
-    graph[s-'a'][e-'a'] = w;
-    graph[e-'a'][s-'a'] = w;
+  for(int i=0;i<M;i++) {
+    in>>s>>e>>w;
+    graph[e-'a'][s-'a']=graph[s-'a'][e-'a']=w;
   }
 
   dfs(0,0,"a");
 
-  out << ans.size() << "\n";
+  out<<ans.size()<<"\n";
   for(char& ch : ans) {
-    out << ch << " ";
+    out<<ch<<" ";
   } 
-  
   return 0;
 }
